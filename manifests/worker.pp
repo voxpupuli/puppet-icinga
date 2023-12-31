1d91e9336f74b46c2deaed88a89dd8ae24252c4b
Lennart Betz <lennart.betz@netways.de>
2023-12-31 11:09:49 +0100

    Set logging_level to warning by default

--- manifests/worker.pp
+++ manifests/worker.pp
@@ -37,12 +37,12 @@ class icinga::worker (
   Stdlib::Host                       $ca_server,
   String                             $zone,
   Hash[String, Hash]                 $parent_endpoints,
+  Icinga::LogLevel                   $logging_level,
   String                             $parent_zone          = 'main',
   Hash[String, Hash]                 $colocation_endpoints = {},
   Hash[String, Hash]                 $workers              = {},
   Array[String]                      $global_zones         = [],
   Enum['file', 'syslog', 'eventlog'] $logging_type         = 'file',
-  Optional[Icinga::LogLevel]         $logging_level        = undef,
   Boolean                            $run_web              = false,
 ) {
   # inject parent zone if no parent exists
