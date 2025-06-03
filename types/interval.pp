40d5edf5acba872b7e018723b0c6cc2adba88731
Lennart Betz <lbetz@prefork.de>
2025-06-03 17:44:01 +0200

    Fix missing summary for type interval

--- types/interval.pp
+++ types/interval.pp
@@ -1,2 +1,3 @@
-# A strict type for intervals
+# @summary
+#   A strict type for intervals
 type Icinga::Interval = Variant[Integer[0], Pattern[/\A\d+\.?\d*[d|h|m|s]?\Z/, /\A\$.+\$\Z/, /\A(host|service)\..+/]]
